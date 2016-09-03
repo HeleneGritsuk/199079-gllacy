a3d1bd13a539436029fc53577b83053aa73c207b
Helene Gritsuk <lenagritsuk@inbox.ru>
2016-09-03 19:36:32 +0300

    исправление замечаний

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 
     function initMap() {
-      var map = new google.maps.Map(document.getElementById('map'), {
+      var map = new google.maps.Map(document.querySelector('.map'), {
         zoom: 16,
         center: {
           lat: 59.939380,
--- js/popup.js
+++ js/popup.js
@@ -4,12 +4,13 @@
     var popup = document.querySelector(".modal-content");
     var overlay = document.querySelector(".modal-overlay");
     var close = popup.querySelector(".modal-content-close");
-
+    var login = popup.querySelector("[name=login]");
 
     link.addEventListener("click", function(event) {
       event.preventDefault();
       popup.classList.add("modal-content-show");
       overlay.classList.add("modal-overlay-show");
+      login.focus();
     });
 
     close.addEventListener("click", function(event) {
@@ -23,3 +24,12 @@
       popup.classList.remove("modal-content-show");
       overlay.classList.remove("modal-overlay-show");
     });
+    
+    window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+          if (popup.classList.contains("modal-content-show")) {
+            popup.classList.remove("modal-content-show");
+            overlay.classList.remove("modal-overlay-show");
+          }
+        }
+      });
